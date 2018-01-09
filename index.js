fef22852e69eaec0b41c353af0200dfc80e01869
Jo Christian Sandvik <jochristian.sandvik@gmail.com>
2018-01-09 18:28:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!";
   }
   else if (string.toUpperCase() === string) {
-    return ``
+    return "YES INDEED!";
+  }
+  else if (string === "I love you, Grandma.") {
+    return ""
   }
 }
